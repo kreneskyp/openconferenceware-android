372010719d48b03d50f9ec17f5c1811866915122
Peter Krenesky <peter@osuosl.org>
2010-05-25 17:40:37 -0700

    added Flipper for sliding between event list and detail.  Added detail view, and handler for loading it

--- src/org/osb/OpenSourceBridgeSchedule.java
+++ src/org/osb/OpenSourceBridgeSchedule.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -19,25 +18,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class OpenSourceBridgeSchedule extends Activity {
-	/** Called when the activity is first created. */
+	
 	EventAdapter mAdapter;
-
+	ViewFlipper mFlipper;
+	Animation mInLeft;
+    Animation mInRight;
+    Animation mOutLeft;
+    Animation mOutRight;
+	
+    TextView mTitle;
+    TextView mTime;
+    TextView mLocation;
+    WebView mDescription;
+    
+    private static final String SCHEDULE_URI = "http://opensourcebridge.org/events/2010/schedule.ics";
+    
+    
+    /** Called when the activity is first created. */
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        
+        mFlipper = (ViewFlipper) findViewById(R.id.flipper);
+        Context context = getApplicationContext();
+        mInLeft = AnimationUtils.loadAnimation(context, R.anim.slide_in_left);
+        mInRight = AnimationUtils.loadAnimation(context, R.anim.slide_in_right);
+        mOutLeft = AnimationUtils.loadAnimation(context, R.anim.slide_out_left);
+        mOutRight = AnimationUtils.loadAnimation(context, R.anim.slide_out_right);
+        
+        // grab views for details
+        View detail = findViewById(R.id.detail); 
+        mTitle = (TextView) detail.findViewById(R.id.title);
+        mTime = (TextView) detail.findViewById(R.id.time);
+        mLocation = (TextView) detail.findViewById(R.id.location);
+        mDescription = (WebView) detail.findViewById(R.id.description);
+        
+        
+        
         InputStream is = null;
         URLConnection conn = null;
 		try {
-			URL url = new URL("http://opensourcebridge.org/events/2010/schedule.ics");
+			URL url = new URL(SCHEDULE_URI);
 		    conn = url.openConnection(); 
 		    conn.setDoInput(true); 
 		    conn.setUseCaches(false);
@@ -50,18 +83,20 @@ public class OpenSourceBridgeSchedule extends Activity {
 	        events.setAdapter(mAdapter);
 			events.setOnItemClickListener(new ListView.OnItemClickListener() {
 				public void onItemClick(AdapterView<?> adapterview, View view, int position, long id) {
-					Event event = (Event) adapterview.getSelectedItem();
-					
-					LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    View v = vi.inflate(R.layout.eventdetail, null);
-					
-                    //WebView webview = (WebView) v.findViewById(R.id.content);
-                    //webview.loadData("testing <a href='http://google.com'> html</a>!", "text/html", "UTF-8");
-                    
-					setContentView(v);
+					Event event = (Event) adapterview.getAdapter().getItem(position);
+					mTitle.setText(event.title);
+					mLocation.setText(event.location);
+					DateFormat formatter = new SimpleDateFormat("HH:mm");
+					String timeString = formatter.format(event.start) + " - " + formatter.format(event.end);
+					mTime.setText(timeString);
+					mDescription.loadData(event.description, "text/html", "UTF-8");
+					mFlipper.setInAnimation(mInRight);
+                    mFlipper.setOutAnimation(mOutLeft);
+                    mFlipper.showNext();
 				}
 			});
             
+			// parse the proposals json to get additional fields
 			ParseProposals(calendar);
 			
 						
@@ -102,7 +137,8 @@ public class OpenSourceBridgeSchedule extends Activity {
 				Event event = events.get(i);
 				if (json.has(event.id)){
 					JSONObject json_event = json.getJSONObject(event.id);
-					event.description = json_event.getString("description");
+					event.description = json_event.getString("description")
+											.replace("\r\n","<br/>");
 				}
 			}
 			
@@ -119,8 +155,11 @@ public class OpenSourceBridgeSchedule extends Activity {
 		}
 	}
 	
+	/**
+	 * EventAdapter used for displaying a list of events
+	 *
+	 */
 	private class EventAdapter extends ArrayAdapter<Event> {
-
 		private ArrayList<Event> items;
 
 		public EventAdapter(Context context, int textViewResourceId,
