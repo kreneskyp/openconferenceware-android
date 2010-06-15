0dc14dff97a9059a6030c449dd4cca5955d332d6
Peter Krenesky <peter@osuosl.org>
2010-06-15 11:24:29 -0700

    removing unused functions for track details from Event

--- src/org/osb/Event.java
+++ src/org/osb/Event.java
@@ -1,16 +1,9 @@
 package org.osb;
 import java.util.Date;
 
-import org.osb.R;
-
 import com.google.gson.annotations.SerializedName;
 
 public class Event {
-	private static final int TRACK_BUSINESS = 7;
-	private static final int TRACK_CHEMISTRY = 8;
-	private static final int TRACK_COOKING = 9;
-	private static final int TRACK_CULTURE = 10;
-	private static final int TRACK_HACKS = 11;
 	
 	@SerializedName("start_time")
 	public Date start;
@@ -43,63 +36,6 @@ public class Event {
 		speaker_ids = new Integer[0];
 		user_titles = new String[0];
 	}
-	
-	public String getTrackName() {
-		switch(track) {
-		case TRACK_BUSINESS:
-			return "Business";
-		case TRACK_CHEMISTRY:
-			return "Chemistry";
-		case TRACK_COOKING:
-			return "Cooking";
-		case TRACK_CULTURE:
-			return "Culture";
-		case TRACK_HACKS:
-			return "Hacks";
-		default:
-			return "";
-		}
-	}
-	
-	/**
-	 * @return the resource id for the track color
-	 */
-	public int getTrackColor() {
-		switch(track) {
-		case TRACK_BUSINESS:
-			return R.color.track_business;
-		case TRACK_CHEMISTRY:
-			return R.color.track_chemistry;
-		case TRACK_COOKING:
-			return R.color.track_cooking;
-		case TRACK_CULTURE:
-			return R.color.track_culture;
-		case TRACK_HACKS:
-			return R.color.track_hacks;
-		default:
-			return R.color.track_other;
-		}
-	}
-	
-	/**
-    * @return the resource id for the track color darker shade
-    */
-    public int getTrackColorDark() {
-	   switch(track) {
-	   case TRACK_BUSINESS:
-	           return R.color.track_business_dark;
-	   case TRACK_CHEMISTRY:
-	           return R.color.track_chemistry_dark;
-	   case TRACK_COOKING:
-	           return R.color.track_cooking_dark;
-	   case TRACK_CULTURE:
-	           return R.color.track_culture_dark;
-	   case TRACK_HACKS:
-	           return R.color.track_hacks_dark;
-	   default:
-	           return R.color.track_other_dark;
-	   }
-    }
     
     @Override
     public String toString()
