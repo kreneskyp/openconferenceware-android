d177580a68fe7d52bda1c15fcc06ebb4e4ff0008
Peter Krenesky <peter@osuosl.org>
2010-06-15 10:37:32 -0700

    adding function to generate list of dates in the conference

--- src/org/osb/Conference.java
+++ src/org/osb/Conference.java
@@ -7,4 +7,27 @@ public class Conference {
 	public Date start;
 	public Date end;
 	public HashMap<Integer, Track> tracks = new HashMap<Integer, Track>();
+	
+	/**
+	 * creates array of dates from start and end date of conference
+	 * @return
+	 */
+	public Date[] getDates() {
+		int length = 0;
+		int day = start.getDate();
+		int month = start.getMonth();
+		int year = start.getYear();
+		
+		if (month == end.getMonth()) {
+			length = end.getDate() - day+1;
+		}
+		
+		Date[] dates = new Date[length];
+		
+		for(int i=0; i<length; i++) {
+			dates[i] = new Date(year, month, day+i);
+		}
+		
+		return dates;
+	}
 }
