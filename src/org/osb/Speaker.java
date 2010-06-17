a1a714379c1d6c13ef16e161f806cc0735b166fb
Peter Krenesky <peter@osuosl.org>
2010-06-17 14:05:43 -0700

    Major update to object loading and caching:    After objects are deserialized from json they are serialized using the java object serializer and written to a file in that form.  This speeds up loading by 50-65% compared to gson.
    To support this change:
       * all data objects now implement serializable
       * getObject has been rewritten to contain the caching&fallback logic
       * getLocal() and getRemote() now load from the respective locations
       * getURL() no longer writes the data to a file, this will take place in getRemote().  caching now occurs only after
         the json has been deserialized successfully to prevent caching of 404s and other invalid responses.

--- src/org/osb/Speaker.java
+++ src/org/osb/Speaker.java
@@ -1,6 +1,9 @@
 package org.osb;
 
-public class Speaker {
+import java.io.Serializable;
+
+public class Speaker  implements Serializable{
+	private static final long serialVersionUID = 7599568491847672394L;
 	String name = null;
 	String biography = null;
 	String twitter = null;
